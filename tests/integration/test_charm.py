bca50114c11f797bb1b2437ea2e147c8ddd14f9d
Natasha Ho <natasha.ho@canonical.com>
2022-10-13 16:30:21 -0400

    initial integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
+
+import logging
+import urllib.request
+from pathlib import Path
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = METADATA["name"]
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest):
+    """Build the charm-under-test and deploy it.
+    Assert on the unit status before any relations/configurations take place.
+    """
+    charm = await ops_test.build_charm(".")
+    resources = {"oci-image": METADATA["resources"]["oci-image"]["upstream-source"]}
+    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, trust=True)
+
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="active",
+            raise_on_blocked=True,
+            timeout=1000,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
