ced26ef606d52791bb307c0e406add85779277e0
Natasha Ho <natasha.ho@canonical.com>
2022-11-21 18:44:48 -0500

    update integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
+POSTGRES = "postgresql-k8s"
 
 
 @pytest.mark.abort_on_fail
@@ -21,13 +22,21 @@ async def test_build_and_deploy(ops_test: OpsTest):
 
     Assert on the unit status before any relations/configurations take place.
     """
+    await ops_test.model.deploy(
+        POSTGRES,
+        channel="latest/edge",
+        trust=True,
+    )
     charm = await ops_test.build_charm(".")
     resources = {"oci-image": METADATA["resources"]["oci-image"]["upstream-source"]}
-    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, trust=True)
+    await ops_test.model.deploy(
+        charm, resources=resources, application_name=APP_NAME, trust=True, series="jammy"
+    )
+    await ops_test.model.add_relation(APP_NAME, POSTGRES)
 
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(
-            apps=[APP_NAME],
+            apps=[APP_NAME, POSTGRES],
             status="active",
             raise_on_blocked=True,
             timeout=1000,
