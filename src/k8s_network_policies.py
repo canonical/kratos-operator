c72be88ff566d93dcf9ec6a3b19dc6a34d71ca7b
Nikos <nikos.sklikas@canonical.com>
2023-04-20 10:33:36 +0300

    Fix dns

--- src/k8s_network_policies.py
+++ src/k8s_network_policies.py
@@ -81,10 +81,14 @@ class K8sNetworkPoliciesHandler:
             metadata=ObjectMeta(name=name),
             spec=NetworkPolicySpec(
                 podSelector=LabelSelector(
-                    matchLabels={"app.kubernetes.io/name": self._charm.app.name}
+                    matchLabels={
+                        "app.kubernetes.io/name": self._charm.app.name,
+                        "kubernetes.io/metadata.name": self._charm.model.name,
+                    }
                 ),
-                policyTypes=["Ingress"],
+                policyTypes=["Ingress", "Egress"],
                 ingress=ingress,
+                egress=[{}]
             ),
         )
 
