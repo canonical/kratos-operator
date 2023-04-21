51b8643c2c25e3c8df365ba075b6f253ddab07f5
Nikos <nikos.sklikas@canonical.com>
2023-04-21 11:55:06 +0300

    Fix tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -51,26 +51,11 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
 
     Assert on the unit status before any relations/configurations take place.
     """
-    await ops_test.model.deploy(
-        POSTGRES,
-        channel="latest/edge",
-        trust=True,
-    )
     charm = await ops_test.build_charm(".")
     resources = {"oci-image": METADATA["resources"]["oci-image"]["upstream-source"]}
     await ops_test.model.deploy(
         charm, resources=resources, application_name=APP_NAME, trust=True, series="jammy"
     )
-    await ops_test.model.add_relation(APP_NAME, POSTGRES)
-
-    async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(
-            apps=[APP_NAME, POSTGRES],
-            status="active",
-            raise_on_blocked=True,
-            timeout=1000,
-        )
-        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
 
 async def test_ingress_relation(ops_test: OpsTest, client: Client) -> None:
@@ -99,6 +84,24 @@ async def test_ingress_relation(ops_test: OpsTest, client: Client) -> None:
     # Validate network policies are created when ingress is provided
     policy = client.get(NetworkPolicy, "kratos-network-policy", namespace=ops_test.model.name)
     assert policy
+
+
+async def test_postgres_relation(ops_test: OpsTest) -> None:
+    await ops_test.model.deploy(
+        POSTGRES,
+        channel="14/stable",
+        trust=True,
+    )
+    await ops_test.model.add_relation(APP_NAME, POSTGRES)
+
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME, POSTGRES],
+            status="active",
+            raise_on_blocked=True,
+            timeout=1000,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
 
 async def test_has_public_ingress(ops_test: OpsTest) -> None:
