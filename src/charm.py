91d28ffe0dfbb1f3411897754d62d96c74497709
bencekov <bence.kovacs.business@gmail.com>
2023-06-07 18:41:25 -0600

    fix: minor change in var self._log_path

--- src/charm.py
+++ src/charm.py
@@ -107,8 +107,8 @@ class KratosCharm(CharmBase):
         self._prometheus_scrape_relation_name = "metrics-endpoint"
         self._loki_push_api_relation_name = "logging"
         self._kratos_service_command = "kratos serve all"
-        self._log_path = "/var/log/kratos.log"
         self._log_dir = "/var/log"
+        self._log_path = f"{self._log_dir}/kratos.log"
 
         self.kratos = KratosAPI(
             f"http://127.0.0.1:{KRATOS_ADMIN_PORT}", self._container, str(self._config_file_path)
