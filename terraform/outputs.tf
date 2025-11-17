a9c108729f7a3dde4b2169375aaab1dcc47c8cf4
shipperizer <alexcabb@gmail.com>
2025-11-17 10:58:00 +0000

    fix: switch to use -route relations in the tf module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,9 +10,8 @@ output "requires" {
   description = "The Juju integrations that the charm requires"
   value = {
     pg-database         = "pg-database"
-    public-ingress      = "public-ingress"
-    admin-ingress       = "admin-ingress"
-    internal-ingress    = "internal-ingress"
+    public-route        = "public-route"
+    internal-route      = "internal-route"
     kratos-external-idp = "kratos-external-idp"
     hydra-endpoint-info = "hydra-endpoint-info"
     ui-endpoint-info    = "ui-endpoint-info"
